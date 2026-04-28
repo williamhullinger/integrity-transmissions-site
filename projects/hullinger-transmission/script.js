ce54150a29356c027041c080ee656cacdf19c166
William Hullinger <your-email@example.com>
2026-04-27 23:44:29 -0500

    remove: extra tobble script

--- projects/hullinger-transmission/script.js
+++ projects/hullinger-transmission/script.js
@@ -1,19 +1,4 @@
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('#site-nav');
 
-if (toggle && nav) {
-  toggle.addEventListener('click', function () {
-    nav.classList.toggle('is-open');
-
-    const isOpen = nav.classList.contains('is-open');
-
-    toggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
-    toggle.setAttribute(
-      'aria-label',
-      isOpen ? 'Close navigation' : 'Open navigation'
-    );
-  });
-}
 
 
 const navToggle = document.querySelector(".nav-toggle");
